0eb644e1d5f75aef098ba2ac198ff1430bab99b3
ApolloZhu <public-apollonian@outlook.com>
2018-03-11 01:13:25 -0500

    iOS - Needs Fundamentals

---
+++ iOS/Grasp/Model/Need.swift
@@ -0,0 +1,14 @@
+//
+//  Need.swift
+//  Grasp
+//
+//  Created by Apollo Zhu on 3/11/18.
+//  Copyright © 2018 Grasp. All rights reserved.
+//
+
+import Foundation
+
+struct Need: Codable {
+    let name: String
+    let cost: Double
+}
